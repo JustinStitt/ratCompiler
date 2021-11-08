54cde74507bdcc967c7ecc1bca61b51041f94572
Justin Stitt <jkstitt@csu.fullerton.edu>
2021-11-07 17:05:42 -0800

    ✅ re-enabled CL args for fn

--- cc-port/main.cc
+++ cc-port/main.cc
@@ -3,16 +3,16 @@
 using namespace LexicalAnalyzer;
 
 int main(int arg, char** args) {
-   /* if(arg < 2) {
+   if(arg < 2) {
         std::cout << "Please provide a filename... (example: \"test.rat\")\n";
         return -1;
     }
     else if(arg > 2) {
         std::cout << "Too many arguments provided.. (expected: {filename})\n"; 
         return -1;
-    } */
+    }
 
-    Parser parser("test3.rat");
+    Parser parser(args[1]);
 
     return 0;
 }
